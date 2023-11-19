7c35855ce42d2acbceaee41c884afdbf453d5a41
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-19 10:34:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function FancyCalculator(num1,num2) {
+	var product = parseFloat(num1)*2;
+	var sum = product+parseFloat(num2);
+	return sum;
+
+	 }
+var number1 =prompt("enter first number");
+var number2 =prompt("enter second number");
+	var result =FancyCalculator(number1,number2);
+alert("The final result is: " + 
+	  result);
